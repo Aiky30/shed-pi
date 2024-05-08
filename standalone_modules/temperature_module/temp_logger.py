95d9f0600afe55cbbda257a93058d5e096a7722d
Andrew Aikman <aiky30@yahoo.com>
2024-05-08 09:43:05 +0100

    Restructure code to allow multiple data submissions and also restructuring tests

--- standalone_modules/temperature_module/temp_logger.py
+++ standalone_modules/temperature_module/temp_logger.py
@@ -13,6 +13,7 @@ TODO:
 - Separate installation script
 - Requests library
 - Login and push
+- Power on is actually Application start!
 """
 
 
@@ -28,6 +29,7 @@ TIME_TO_SLEEP = 60  # time in seconds
 HUB_ADDRESS = "http://localhost:8000"
 # HUB_ADDRESS = "http://192.168.2.130:8000"
 
+MODULE_VERSION = "0.0.1"
 MODULE_POWER_ID = ""
 MODULE_TEMP_ID = ""
 MODULE_PROBE_TEMP_ID = ""
@@ -43,16 +45,40 @@ def get_time():
     return current_time
 
 
-class TempProbe:
+class ReadingSubmissionService:
     def __init__(self):
+        self.base_url: str = HUB_ADDRESS
+        self.data_submission_endpoint: str = "/api/v1/device-module-readings/"
+
+    def submit(self, device_module_id: int, data: dict) -> requests.Response:
+        """
+        Submit a reading to the ShedPi data submittion endpoint
+
+        :param device_module_id: The DeviceModule id
+        :param data: A data payload to submit
+        :return:
+        """
+        logger.info(f"Submitting reading for device: {device_module_id}, {data}")
+
+        endpoint = f"{self.base_url}{self.data_submission_endpoint}"
+        response = requests.post(
+            endpoint,
+            data={"device_module": device_module_id, "data": json.dumps(data)},
+        )
+
+        # TODO: Validate the response
+
+        return response
+
+
+class TempProbe:
+    def __init__(self, submission_service: ReadingSubmissionService):
         self.device_id: int = MODULE_PROBE_TEMP_ID
         # TODO: Make this an env var, it can then be overridden by the tests
-        self.base_url: str = HUB_ADDRESS
-        self.module_reading_endpoint: str = "/api/v1/device-module-readings/"
-
         base_dir = "/sys/bus/w1/devices/"
         device_folder = Path.glob(base_dir + "28*")[0]
         self.device_file = device_folder + "/w1_slave"
+        self.submission_service = submission_service
 
     def read_temp_raw(self) -> list[str]:
         with open(self.device_file, "r") as f:
@@ -84,26 +110,21 @@ class TempProbe:
         """
         probe_1_temp = self.read_temp()
 
-        logger.info(f"Submitting reading: {probe_1_temp}")
-
-        # Get a request client
         # FIXME: Should this be a float or a string? Broke the test
         data = {"temperature": str(probe_1_temp)}
-        endpoint = f"{self.base_url}{self.module_reading_endpoint}"
-        response = requests.post(
-            endpoint,
-            data={"device_module": self.device_id, "data": json.dumps(data)},
-        )
 
-        # TODO: Validate the response
+        response = self.submission_service.submit(
+            device_module_id=self.device_id, data=data
+        )
 
         return response
 
 
 class RPIDevice:
-    def __init__(self):
-        self.device_id = MODULE_POWER_ID
-        self.cpu_module_id = MODULE_TEMP_ID
+    def __init__(self, submission_service: ReadingSubmissionService):
+        self.device_id: int = MODULE_POWER_ID
+        self.cpu_module_id: int = MODULE_TEMP_ID
+        self.submission_service = submission_service
 
     def get_cpu_temp(self):
         cpu_temp = os.popen("vcgencmd measure_temp").readline()
@@ -111,25 +132,68 @@ class RPIDevice:
         # Convert the temp read from the OS to a clean float
         return float(cpu_temp.replace("temp=", "").replace("'C\n", ""))
 
+    def submit_reading(self) -> requests.Response:
+        """
+        Submits a reading to an external endpoint
+
+        :return:
+        """
+        cpu_temp = self.get_cpu_temp()
+
+        # FIXME: Should this be a float or a string? Broke the test
+        data = {"temperature": str(cpu_temp)}
+
+        response = self.submission_service.submit(device_id=self.device_id, data=data)
+
+        return response
+
+    def submit_device_startup(self):
+        logger.info(f"Shed pi started: {get_time()}, using version: {MODULE_VERSION}")
+
+        data = {"power": True}
+        response = self.submission_service.submit(device_id=self.device_id, data=data)
+        return response
+
+    def submit_device_shutdown(self):
+        data = {"power": False}
+        response = self.submission_service.submit(device_id=self.device_id, data=data)
+        return response
+
+
+class DeviceProtocol:
+    def __init__(self, submission_service: ReadingSubmissionService):
+        # Installed modules
+        submission_service = ReadingSubmissionService()
+        self.temp_probe = TempProbe(submission_service=submission_service)
+        self.rpi_device = RPIDevice(submission_service=submission_service)
+
+    def startup(self):
+        self.rpi_device.submit_device_startup()
+
+    def run(self):
+        while True:
+            # TODO: Would be nice to be able to bundle multiple calls into 1, less of an issue initially
+            self.temp_probe.submit_reading()
+            self.rpi_device.submit_reading()
+
+            time.sleep(TIME_TO_SLEEP)
+
+    def shutdown(self):
+        self.rpi_device.submit_device_shutdown()
+
 
 def main():
-    logger.info(f"Shed pi started: {get_time()}, using version: 0.0.1")
-
     if not check_os():
         logger.error("only rasbian os supported")
         return
 
-    temp_probe = TempProbe()
-    rpi_device = RPIDevice()
+    device = DeviceProtocol()
 
-    while True:
-        pi_temp = rpi_device.get_cpu_temp()
-
-        logger.info(f"Pi temp: {pi_temp}")
-
-        temp_probe.submit_reading()
-
-        time.sleep(TIME_TO_SLEEP)
+    try:
+        device.startup()
+        device.run()
+    finally:
+        device.shutdown()
 
 
 if __name__ == "__main__":
--- standalone_modules/temperature_module/test_temp_module.py
+++ standalone_modules/temperature_module/test_temp_module.py
@@ -9,13 +9,13 @@ from shedpi_hub_dashboard.tests.utils.factories import (
     DeviceModuleFactory,
 )
 
-from .temp_logger import TempProbe
+from .temp_logger import ReadingSubmissionService, TempProbe
 
 
 @patch("standalone_modules.temperature_module.temp_logger.Path")
 def test_temp_probe_reading_happy_path(mocked_path):
     # FIXME: Get the actual readout from the modules
-    probe = TempProbe()
+    probe = TempProbe(submission_service=Mock())
     probe.read_temp_raw = Mock(
         return_value=[
             "YES",
@@ -34,7 +34,7 @@ def test_temp_probe_reading_invalid_reading(mocked_path):
     - Find what a real invalid reading looks like
     """
     # FIXME: Get the actual readout from the modules
-    probe = TempProbe()
+    probe = TempProbe(submission_service=Mock())
     probe.read_temp_raw = Mock(
         return_value=[
             "YES",
@@ -61,7 +61,7 @@ def test_temp_probe_reading_invalid_reading_missing_expected_params(mocked_path)
         "YES",
         "t=2000",
     ]
-    probe = TempProbe()
+    probe = TempProbe(submission_service=Mock())
     probe.read_temp_raw = Mock(side_effect=[try_1, try_2])
 
     temp = probe.read_temp()
@@ -85,10 +85,14 @@ def test_temperature_module_reading_submission(mocked_path, live_server):
         },
     }
     device_module = DeviceModuleFactory(schema=schema)
+    submission_service = ReadingSubmissionService()
+    # Override the serviuce url
+    submission_service.base_url = live_server.url
 
-    probe = TempProbe()
+    probe = TempProbe(submission_service=submission_service)
+    # Override the module id
     probe.device_id = device_module.id
-    probe.base_url = live_server.url
+
     probe.read_temp_raw = Mock(
         return_value=[
             "YES",
@@ -107,4 +111,8 @@ def test_temperature_module_reading_submission(mocked_path, live_server):
     assert DeviceModuleReading.objects.filter(device_module=device_module).count() == 1
 
 
+# TODO: General Integration test for the logger using a fake module
 # TODO: Test default endpoint address settings work in theory, because the test above overrides them
+
+# TODO: Device Protocol needs an integration test
+# Device startup and shutdown needs a unit test
